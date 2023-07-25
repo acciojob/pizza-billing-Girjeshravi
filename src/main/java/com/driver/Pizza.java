c5e568d68f7053adbec6b552cb9f0a39f0476492
Girjeshravi <gwc.ravi52@gmail.com>
2023-07-25 20:15:16 +0530

    code changed

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,59 @@
 package com.driver;
 
 public class Pizza {
-
-    private int price;
     private Boolean isVeg;
     private String bill;
+    private int basePrice;
+    private boolean echeese;
+    private boolean  eToppings;
+    private boolean paperBag;
+    private int totalPrice;
+    private int toppingsPrice;
+    private int paperBagPrice;
+    private int cheesePrice;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg){
+            basePrice=300;
+            toppingsPrice=70;
+        }else{
+            basePrice=400;
+            toppingsPrice=120;
+        }
+        paperBagPrice=20;
+        cheesePrice=80;
     }
 
     public int getPrice(){
-        return this.price;
+        return this.totalPrice;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if(!echeese){
+            bill="extra chees: "+cheesePrice+ "/n";
+            echeese=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!eToppings){
+            bill=bill+"extra tooping: "+ toppingsPrice+"/n";
+            eToppings=false;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        bill=bill+"paper bag price: "+paperBagPrice+"/n";
+        paperBag=true;
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        return "Pizza base price "+this.basePrice +" "+ this.bill;
     }
 }
